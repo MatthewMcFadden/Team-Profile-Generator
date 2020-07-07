da2e48fd37a73786e96ac516d7ac7f055b930c08
Matt M <matt.mcfadden14@gmail.com>
2020-07-06 18:59:52 -0600

    Employee.test.js intialized

---
+++ tests/Employee.test.js
@@ -0,0 +1,32 @@
+const Employee = require(/* file structure ../lib/Employee */);
+
+test("new employee", () => {
+});
+
+test("name", () => {
+
+});
+
+test("id", () => {
+
+});
+
+test("email", () => {
+
+});
+
+test("getName", () => {
+
+});
+
+test("getId", () => {
+
+});
+
+test("getEmail", () => {
+
+});
+
+test("getRole", () => {
+  // returns Employee
+});
