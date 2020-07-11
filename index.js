09ccc15d4d31d9e87d5169a8df5cf09ed4a5b268
Matt M <matt.mcfadden14@gmail.com>
2020-07-11 15:23:09 -0600

    working application. Just need to fine tune

--- index.js
+++ index.js
@@ -1,74 +1,117 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const util = require("util");
+const html = require("./src/page-template");
 
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
+const writeFileAsync = util.promisify(fs.writeFile);
+const appendFileAsync = util.promisify(fs.appendFile);
+
 let team = [];
+let generateTeam = '';
+
+async function app () {
+  try {
+    await teamPrompt ()
+    for (let i = 0; i < team.length; i++) {
+      generateTeam = generateTeam + html.generateCard(team[i]);
+    }
+
+    let finalHTML = html.generateHTML(generateTeam)
+
+    console.log(generateTeam);
+
+    writeFileAsync("./dist/index.html", finalHTML)
+    
+  } catch (err) {
+    return console.log(err);
+  }
+};
 
 async function teamPrompt () {
-  response = await inquirer.prompt ([
-    {
-      type: "input",
-      name: "name",
-      message: "What is the employee's name?: "
-    },
+  let finishedResponse = "";
+  do {
+    try {
+      response = await inquirer.prompt ([
+        {
+          type: "input",
+          name: "name",
+          message: "What is the employee's name?: "
+        },
 
-    {
-      type: "input",
-      name: "id",
-      message: "Enter the employee's ID number: "
-    },
+        {
+          type: "input",
+          name: "id",
+          message: "Enter the employee's ID number: "
+        },
 
-    {
-      type: "input",
-      name: "email",
-      message: "Enter the employee's email address: "
-    },
+        {
+          type: "input",
+          name: "email",
+          message: "Enter the employee's email address: "
+        },
 
-    {
-      type: "list",
-      name: "role",
-      message: "What is the employee's role?: ",
-      choices: [
-        "Manger",
-        "Engineer",
-        "Intern"
-      ]
+        {
+          type: "list",
+          name: "role",
+          message: "What is the employee's role?: ",
+          choices: [
+            "Manager",
+            "Engineer",
+            "Intern"
+          ]
+        }
+      ]);
+
+      let secondResponse = ""
+
+      if (response.role === "Manager") {
+        secondResponse = await inquirer.prompt ([{
+          type: "input",
+          name: "x",
+          message: "What is the employee's office number?: "
+        }]);
+        // store information and push
+        const manager = new Manager (response.name, response.id, response.email, secondResponse.x);
+        team.push(manager);  
+
+      } else if (response.role === "Engineer") {
+        secondResponse = await inquirer.prompt ([{
+          type: "input",
+          name: "x",
+          message: "What is the employee's Github username?: "
+        }]);
+        // store information and push
+        const engineer = new Engineer (response.name, response.id, response.email, secondResponse.x);
+        team.push(engineer);
+
+      } else if (response.role === "Intern") {
+        secondResponse = await inquirer.prompt ([{
+          type: "input",
+          name: "x",
+          message: "What school is this employee attending?: "
+        }]);
+        // store information and push
+        const intern = new Intern (response.name, response.id, response.email, secondResponse.x);
+        team.push(intern);
+      };
+    } catch (err) {
+      return console.log(err);
     }
-  ]);
 
-  let response = ""
-
-  if (response.role === "Manager") {
-    response = await inquirer.prompt ([{
-      type: "input",
-      name: "x",
-      message: "What is the employee's office number?: "
-    }]);
-    // store information and push
-    const manager = new Manager (response.name, response.id, response.email, response.x);
-    team.push(manager);  
-
-  } else if (response.role === "Engineer") {
-    response = await inquirer.prompt ([{
-      type: "input",
-      name: "x",
-      message: "What is the employee's Github username?: "
-    }]);
-    // store information and push
-    const engineer = new Engineer (response.name, response.id, response.email, response.x);
-    team.push(engineer);
-
-  } else if (response.role === "Intern") {
-    response = await inquirer.prompt ([{
-      type: "input",
-      name: "x",
-      message: "What school is this employee attending?: "
-    }]);
-    // store information and push
-    const intern = new Intern (response.name, response.id, response.email, response.x);
-    team.push(intern);
-  };
+    finishedResponse = await inquirer.prompt([{
+      type: "list",
+      name: "finish",
+      message: "Do you want to add more team members?: ",
+      choices: [
+        "Yes",
+        "No"
+      ]
+    }])
+  } while (finishedResponse.finish === "Yes");
 }
+
+app ();
---
+++ src/page-template.js
@@ -0,0 +1,91 @@
+const generateHTML = function (generateTeam) {
+  console.log("Inside gen html");
+  return `<!DOCTYPE html>
+  <html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Team Portfolio Generator</title>
+    <style>
+      body {
+        background: url('../assets/images/computer-coding.jpeg') center;
+        background-size: 100% 100%;
+        background-repeat: no-repeat;
+        margin: 0%;
+        height: 100vh;
+      }
+
+      .header {
+        background-color: rgb(63, 82, 251);
+        border: solid black;
+        text-align: center;
+        font-size: 30px;
+        font-family: Arial, Helvetica, sans-serif;
+      }
+
+      .container-body {
+        display: flex;
+        justify-content: space-evenly;
+      }
+
+      .card {
+        background-color:rgb(255, 255, 255);
+        margin-top: 4%;
+        border: solid black;
+        font-size: x-large;
+      }
+
+      .card-header{
+        margin: 10%;
+      }
+
+      .card-body{
+        margin: 5%;
+      }
+    </style>
+  </head>
+
+  <body>
+    <div class=header>
+      <h1>My Development Team</h1>
+    </div>
+    <div class="container-body">
+      ${generateTeam}
+    </div>
+    <script src="https://kit.fontawesome.com/257de25400.js" crossorigin="anonymous"></script>         
+  </body>
+  </html>`
+}
+
+const generateCard = function (arr) {
+  //if else statement
+  let roleInfo;
+
+  if (arr.title === "Manager") {
+      roleInfo = `Office Number: ${arr.officeNumber}`
+  } else if (arr.title === "Engineer") {
+      roleInfo = `Github Username: ${arr.github}`
+  } else if (arr.title === "Intern") {
+      roleInfo = `School: ${arr.school}`
+  }
+
+  return `<div class="card">
+    <div class="card-header">
+      <h2>${arr.name}</h2>  
+      <h2><i class="far fa-user"></i> ${arr.title}</h2>
+      <hr>
+    </div>
+    <div class="card-body">
+      <ul>
+        <li>ID: ${arr.id}</li>
+        <li>Email: ${arr.email}</li>
+        <li>${roleInfo} </li>
+      </ul>
+    </div>
+  </div>`
+}
+
+exports.generateHTML = generateHTML
+exports.generateCard = generateCard;
